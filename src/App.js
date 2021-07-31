4c690e821974b11561c2ed598e6409a967a59c1c
Leo Diaz <leoangelo.diaz@gmail.com>
2021-08-01 02:45:12 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '@/logo.svg';
+import mugshot from "@/assets/images/leod-mugshot.png"
+import '@/App.css';
 
-function App() {
+import "@/assets/style.scss"
+
+const App = () => {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container-fluid px-0 position-relative">
+        <div className="row no-gutters">
+          <div className="section-header-content">
+            <img src={mugshot} />
+            <h1 className="display-4">Leo Diaz</h1>
+            <h5 className="text-light">Full-Stack Developer</h5>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+};
 
 export default App;
