9e0eb3f25385208d4a48d0c297c8a3d3991cf446
Leo Diaz <leoangelo.diaz@gmail.com>
2021-08-01 03:03:42 +0800

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@/index.css';
+
 import App from '@/App';
 import reportWebVitals from './reportWebVitals';
+
+import "@/assets/cover.scss"
+import "@/assets/style.scss"
 
 ReactDOM.render(
   <React.StrictMode>
